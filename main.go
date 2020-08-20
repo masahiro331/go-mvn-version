df1a0aae603d17bd764b7cd728fea794c35af134
masahiro331 <mur4m4s4.331@gmail.com>
2020-08-21 00:25:16 +0900

    update main.go

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 )
 
 func main() {
-	v1, err := version.NewVersion("1.alpha")
+	v1, err := version.NewVersion("10-snapshot")
 	if err != nil {
 		log.Fatal(err)
 	}
-	v2, err := version.NewVersion("1")
+	v2, err := version.NewVersion("10-b1")
 	if err != nil {
 		log.Fatal(err)
 	}
