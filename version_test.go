bd17d24e7e5460b6b475d8a2493c89487eac1377
knqyf263 <knqyf263@gmail.com>
2020-09-27 18:52:33 +0300

    test: add another failing case

--- version_test.go
+++ version_test.go
@@ -97,6 +97,11 @@ func TestEqual(t *testing.T) {
 			v2:     "1-1-ga",
 			expect: true,
 		},
+		{
+			v1:     "1-1.ga",
+			v2:     "1-1.0",
+			expect: true,
+		},
 	}
 	for i, testCase := range testCases {
 		v1, err := version.NewVersion(testCase.v1)
