0cc392b762f12f51bee8be6a6116b9a3fe126c3c
masahiro331 <mur4m4s4.331@gmail.com>
2021-04-29 23:45:54 +0900

    refactor(compare) use multierror

--- comparer.go
+++ comparer.go
@@ -1,19 +1,28 @@
 package version
 
-import "golang.org/x/xerrors"
+import (
+	multierror "github.com/hashicorp/go-multierror"
+	"golang.org/x/xerrors"
+)
 
 type Comparer interface {
 	Check(v Version) bool
 }
 
 func NewComparer(v string) (Comparer, error) {
+	var errs error
+
 	c, err := NewConstraints(v)
 	if err == nil {
 		return c, nil
 	}
+	errs = multierror.Append(errs, err)
+
 	r, err := NewRequirements(v)
 	if err == nil {
 		return r, nil
 	}
-	return nil, xerrors.Errorf("failed to new comparer: %w", err)
+	errs = multierror.Append(errs, err)
+
+	return nil, xerrors.Errorf("failed to new comparer: %w", errs)
 }
